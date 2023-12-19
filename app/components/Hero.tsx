2aaeeb4535cdc6e68444e8362d6d6b1e0a208dc5
JonathanAEG <jonathan.ramos@ikasle.aeg.eus>
2023-12-19 20:26:28 +0100

    [ADD] Añadida sección de Imagenes en el componente Hero

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,5 +1,16 @@
+import { client, urlFor } from "../lib/sanity";
+import Image from 'next/image';
 
-export default function Hero(){
+const getData = async ()=>{
+    const query = "*[_type == 'heroImage'][0]"
+    
+    return await client.fetch(query);
+    
+}   
+
+export default async function Hero(){
+
+    const data = await getData();
 
     return(
         <section className="mx-auto max-w-2xl px-4 sm:pb-6 lg:max-w-7xl lg:px-8" >
@@ -14,6 +25,33 @@ export default function Hero(){
                         We are the best so come and shop with us.
                     </p>
                 </div>
+
+                <div className="mb-12 flex w-full md:mb-16 lg:w-2/3">
+                    <div className="relative left-12 top-12 z-10 -ml-12 overflow-hidden rounded-lg bg-gray-100 shadow-lg md:left-16 md:top-16 lg:ml-0">
+                        <Image
+                            src={urlFor(data.image2).url()}
+                            className="h-full object-cover object-center"
+                            width={500}
+                            height={500}
+                            alt="Photo"
+                            priority
+                        />
+                    </div>
+
+                    <div className="overflow-hidden rounded-lg bg-gray-100 shadow-lg">
+                    <Image
+                            src={urlFor(data.image1).url()}
+                            className="h-full object-cover object-center"
+                            width={500}
+                            height={500}
+                            alt="Photo"
+                            priority
+                        />
+                    </div>
+                </div>
+                
+                
+
             </div>
         </section>
     ) 
