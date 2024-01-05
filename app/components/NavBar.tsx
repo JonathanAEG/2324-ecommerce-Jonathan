b83d3098f2390c271a173f29459e73a38dc674e5
JonathanAEG <jonathan.ramos@ikasle.aeg.eus>
2024-01-05 18:36:54 +0100

    [ADD] Añadida funcionalidad en botón de Cart que enseña la lista de la compra

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -3,6 +3,7 @@ import { Button } from '@/components/ui/button';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import {ShoppingBag} from 'lucide-react';
+import { useShoppingCart } from 'use-shopping-cart';
 
 const  links = [
     {name: 'Home', href:'/'},
@@ -16,6 +17,7 @@ export default function NavBar(){
 
     const pathName = usePathname();
 
+    const {handleCartClick} = useShoppingCart();
     return(   
 
         <header className="mb-8 border-b" >
@@ -54,6 +56,7 @@ export default function NavBar(){
                     
                     <Button 
                     variant={'outline'}
+                    onClick={()=>{handleCartClick()}}
                     className='flex flex-col gap-y-1.5 h-12 w-12 sm:h-20 sm:w-20 md:h-24 md:w-24  rounded-none' >
                         <ShoppingBag/>
                         <span className='hidden text-xs font-semibold text-gray-500 sm:block' >
